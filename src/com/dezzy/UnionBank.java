6722919285b6cb2aaa2b4a9b2e1c7d57c4e7c502
Dezzy12 <desmondneba134@gmail.com>
2022-10-15 08:15:08 +0100

    Created a sample banking system in Java

---
+++ src/com/dezzy/UnionBank.java
@@ -0,0 +1,9 @@
+package com.dezzy;
+
+public class UnionBank {
+
+    public static void main(String[] args) {
+        Account Neba = new Account("Neba Desmond", "UB00001");
+        Neba.showMenu();
+    }
+}
